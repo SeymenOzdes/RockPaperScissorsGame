d470404a60261cbd2962f2ce64bf8c43b5889e66
SeymenOzdes <ozdesxseymen@gmail.com>
2023-09-14 14:28:39 +0300

    The data of the application's shape selection, the purpose of the game and the user's choice were received.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -6,16 +6,71 @@
 //
 
 import SwiftUI
+struct ImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .scaledToFit()
+            .clipShape(Capsule())
+            .frame(width: 200, height: 200)
+    }
+}
+extension View {
+    func imageModifier() -> some View {
+        modifier(ImageModifier())
+    }
+}
 
 struct ContentView: View {
+    @State private var shapes = ["Rock✊", "Paper🫱", "Scissor✌️"]
+    @State private var images = ["RockImage", "PaperImage", "BlackScissorImage"]
+    @State private var purposes = ["Win", "Lose"]
+    @State private var selectedShape = Int.random(in: 0...2)
+    @State private var purposeGame = Int.random(in: 0...1) // win or lose randomness olmasını sağlıyor.
+    @State private var userChoice: Int = 4 // I'm not sure/ 4. sırada hiçbir shape yok, başlangıçta no selection yapmak için böyle yaptım.
+    
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                VStack(spacing: 20) {
+                    Section {
+                        VStack {
+                            Text("Selection of system")
+                                .font(.title2.bold())
+                            Text("\(shapes[selectedShape])")
+                                .font(.title.bold())
+                                .foregroundColor(.blue)
+                            Image("\(images[selectedShape])")
+                                .resizable()
+                                .imageModifier()
+                        }
+                    }
+                    Section {
+                        Text("Purpose of the game")
+                            .font(.title2.bold())
+                        Picker("Purpose", selection: $purposeGame) {
+                            ForEach(0..<2) {
+                                Text("\(purposes[$0])")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    Section {
+                        VStack{
+                            Text("make your choice")
+                                .font(.title2.bold())
+                            Picker("UserChoice", selection: $userChoice) {
+                                ForEach(0..<3) {
+                                    Text("\(shapes[$0])")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                    }
+                    Spacer()
+                }
+            }
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.gray)
     }
 }
 
